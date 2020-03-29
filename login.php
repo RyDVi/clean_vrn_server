872dd77255e4af65412eb2b54b115f85d1e76160
RyDVi <dmitrii.kardan.rch@mail.ru>
2020-03-29 10:40:35 +0300

    added user data to response

--- login.php
+++ login.php
@@ -5,24 +5,28 @@ $data = json_decode($postData, true);
 header("Content-Type: application/json");
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     if (isset($data['username']) && isset($data['password'])) {
-        $stmt = $conn->prepare("SELECT id, id_type FROM users WHERE email=? AND password=?");
+        $stmt = $conn->prepare("SELECT id, id_type, firstname, lastname, middlename, email, phone FROM users WHERE email=? AND password=?");
         $password = md5($data["password"]);
         $stmt->bind_param("ss", $data["username"], $password);
         if (!$stmt->execute()) {
             // http_response_code(201);
             json_encode(["error" => $stmt->error]);
         } else {
-            $stmt->bind_result($id, $idUserType);
+            $stmt->bind_result($id, $idUserType, $firstname, $lastname, $middlename, $email, $phone);
             if ($stmt->fetch()) {
                 // http_response_code(201);
-                echo json_encode(['session_id' => 'PHPSESSID=' . session_id(), 'id_user_type' => $idUserType, 'id_user' => $id]);
+                echo json_encode([
+                    'session_id' => 'PHPSESSID=' . session_id(), 'id_user_type' => $idUserType,
+                    'id_user' => $id, 'firstname' => $firstname, 'lastname' => $lastname,
+                    'middlename' => $middlename, 'email' => $email, 'phone' => $phone
+                ]);
             } else {
                 // http_response_code(201);
                 echo json_encode(["error" => "Логин или пароль введен неверно"]);
             }
         }
     } else if (isset($data['is_player'])) {
-        echo json_encode(['session_id' => 'PHPSESSID=' . session_id()]);
+        echo json_encode(['session_id' => 'PHPSESSID=' . session_id(), 'id_user_type' => 3]);
     } else if (!isset($data['username']) && !isset($data['password'])) {
         // http_response_code(201);
         echo json_encode(["error" => "Логин и пароль не введены"]);
