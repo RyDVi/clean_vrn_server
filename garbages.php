3a8dea8113bf34cd9caa52d2e02ecbf1c628ad1c
Sekerator <tagieinullaev@mail.ru>
2020-04-05 13:28:34 +0300

    Вывод garbages по id и без

--- garbages.php
+++ garbages.php
@@ -3,15 +3,38 @@ include("connect_db.php");
 
 header("Content-Type: application/json");
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $stmtGarbages = $conn->prepare("SELECT id, name FROM garbages");
-    $stmtGarbages->execute();
-    $stmtGarbages->bind_result($id, $name);
-    $garbages = [];
-    while ($stmtGarbages->fetch()) {
-        array_push($garbages, ["id" => $id, "name" => $name]);
+    if (!empty($_GET['id'])) {
+        $id_garbages = $_GET['id'];
+        $stmt = $conn->prepare("SELECT * FROM garbages WHERE id=?");
+        $stmt->bind_param("i", $id_garbages);
+        if (!$stmt->execute()) {
+            echoError(5002);
+        } else {
+            $stmt->bind_result($id, $name);
+            if ($stmt->fetch()) {
+                http_response_code(200);
+                echo json_encode([
+                    "id" => $id, "name" => $name
+                ]);
+            } else {
+                echoError(4041);
+            }
+        }
+    } else {
+        $stmt = $conn->prepare("SELECT * FROM garbages");
+        if (!$stmt->execute()) {
+            echoError(5002);
+        }
+        $stmt->bind_result($id, $name);
+        $data = [];
+        while ($stmt->fetch()) {
+            array_push($data, [
+                "id" => $id, "name" => $name
+            ]);
+        }
+        http_response_code(200);
+        echo json_encode($data);
     }
-    http_response_code(200);
-    echo json_encode($garbages);
 } else {
     echoError(4051);
 }
